4b987624ce9839f87cf836dca865cb258849e238
MalgoskaG <ghouga.m@gmail.com>
2017-12-14 21:32:05 +0100

    deleted an unnecessary function

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -73,16 +73,10 @@ public class WordAdapter extends ArrayAdapter<Word>  {
 
         // Find the ImageView in the list_item.xml layout with the ID image.
         ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
-        // Check if an image is provided for this word or not
-        if (currentWord.hasImage()) {
-            // If an image is available, display the provided image based on the resource ID
-            imageView.setImageResource(currentWord.getImageResourceId());
-            // Make sure the view is visible
-            imageView.setVisibility(View.VISIBLE);
-        } else {
-            // Otherwise hide the ImageView (set visibility to GONE)
-            imageView.setVisibility(View.GONE);
-        }
+
+        imageView.setImageResource(currentWord.getImageResourceId());
+
+//
 
         // Set the theme color for the list item
         View textContainer = listItemView.findViewById(R.id.text_container);
