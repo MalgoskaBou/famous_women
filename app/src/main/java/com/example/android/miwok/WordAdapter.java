ab22a4e0b76a535ba8a5098bbd3cfa8384868c53
Melsara <kaxhortz@gmail.com>
2017-12-16 19:52:51 +0100

    Changing the default code into something more meaningful. Changing the logic to display both future name and profession of our women into the details page.

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -59,17 +59,17 @@ public class WordAdapter extends ArrayAdapter<Word>  {
         // Get the {@link Word} object located at this position in the list
         Word currentWord = getItem(position);
 
-        // Find the TextView in the list_item.xml layout with the ID miwok_text_view.
-        TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+        // Find the TextView in the list_item.xml layout with the ID name_text_view.
+        TextView nameTextView = (TextView) listItemView.findViewById(R.id.name_text_view);
         // Get the Miwok translation from the currentWord object and set this text on
         // the Miwok TextView.
-        miwokTextView.setText(currentWord.getMiwokTranslationId());
+        nameTextView.setText(currentWord.getMiwokTranslationId());
 
-        // Find the TextView in the list_item.xml layout with the ID default_text_view.
-        TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+        // Find the TextView in the list_item.xml layout with the ID profession_text_view.
+        TextView professionTextView = (TextView) listItemView.findViewById(R.id.profession_text_view);
         // Get the default translation from the currentWord object and set this text on
         // the default TextView.
-        defaultTextView.setText(currentWord.getDefaultTranslationId());
+        professionTextView.setText(currentWord.getDefaultTranslationId());
 
         // Find the ImageView in the list_item.xml layout with the ID image.
         ImageView imageView = (ImageView) listItemView.findViewById(R.id.image);
