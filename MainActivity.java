e43432dacb360406f043724ac30fb1c7d39d473b
GM <33101796+MalgoskaG@users.noreply.github.com>
2018-01-11 21:56:56 +0100

    Delete MainActivity.java

--- MainActivity.java
+++
@@ -1,6 +0,0 @@
-@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_list_activity, menu);
-        return true;
-    }
