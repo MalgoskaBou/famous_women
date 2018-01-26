65b750fcc117cc6c4cb4db675e35888de6efbb95
Greta Gri <greta.klimaite@gmail.com>
2018-01-26 18:39:06 +0200

    Changed references to correct pictures in DetailsActivity.java.

--- app/src/main/java/com/example/android/miwok/DetailsActivity.java
+++ app/src/main/java/com/example/android/miwok/DetailsActivity.java
@@ -70,11 +70,11 @@ public class DetailsActivity extends AppCompatActivity {
     private void initDetailsArray(){
         // ToDo: Fix wrong Word constructor usage
         details.add(new Word(R.string.maria_profession, R.string.body_details_descriprion_maria_sklodowska,
-                R.drawable.maria_sklodowska_listimg, R.drawable.maria_poland_flag, R.string.maria));
+                R.drawable.maria_portrait, R.drawable.maria_poland_flag, R.string.maria));
         details.add(new Word(R.string.dalia_profession, R.string.body_details_description_dalia,
-                R.drawable.dalia_grybauskaite_listimg, R.drawable.dalia_lithuania_flag, R.string.dalia));
+                R.drawable.dalia_portrait, R.drawable.dalia_lithuania_flag, R.string.dalia));
         details.add(new Word(R.string.elisabeta_proffesion, R.string.body_details_description_elisabeta,
-                R.drawable.elisabeta_rizea_listimg, R.drawable.elisabeta_rizea_flag, R.string.elisabeta));
+                R.drawable.elisabeta_portrait, R.drawable.elisabeta_rizea_flag, R.string.elisabeta));
         details.add(new Word(R.string.mother_theresa_profession,R.string.body_details_description_mother_theresa,
                 R.drawable.theresa_portrait, R.drawable.theresa_macedonia_flag, R.string.mother_theresa));
         details.add(new Word(R.string.wanda_profession, R.string.body_details_description_wanda,
@@ -84,15 +84,15 @@ public class DetailsActivity extends AppCompatActivity {
         details.add(new Word(R.string.sirleaf_profession, R.string.body_details_description_sirleaf,
                 R.drawable.sirleaf_portrait, R.drawable.sirleaf_liberia_flag, R.string.ellen_Sirleaf));
         details.add(new Word(R.string.maria_telkes_profession, R.string.body_details_description_maria_telkes,
-                R.drawable.maria_telkes_listimg, R.drawable.maria_hungary_flag, R.string.maria_telkes));
+                R.drawable.maria_telkes_portrait, R.drawable.maria_hungary_flag, R.string.maria_telkes));
         details.add(new Word(R.string.meriem_profession, R.string.body_details_description_meriem,
                 R.drawable.meriem_portrait, R.drawable.meriem_morocco_flag, R.string.Merieme_Chadid));
         details.add(new Word(R.string.irena_profession, R.string.body_details_description_irena,
                 R.drawable.irena_portrait, R.drawable.maria_poland_flag, R.string.irena));
         details.add(new Word(R.string.ada_profession, R.string.body_details_description_ada,
-                R.drawable.ada_yonath_listimg, R.drawable.israel_flag, R.string.ada));
+                R.drawable.ada_yonath_portrait, R.drawable.israel_flag, R.string.ada));
         details.add(new Word(R.string.ilhan_profession, R.string.body_details_description_ilhan,
-                R.drawable.ilhan_listing, R.drawable.ilhan_flag, R.string.ilhan));
+                R.drawable.ilhan_portrait, R.drawable.ilhan_flag, R.string.ilhan));
         details.add(new Word(R.string.valentina_profession, R.string.body_details_description_valentina,
                 R.drawable.valentina_portrait, R.drawable.russia_flag, R.string.valentina));
     }
