1c1bf6861cf1226194ccb70d61df177181f437f8
GM <ghouga.m@gmail.com>
2018-02-25 17:26:16 +0100

    RecyclerView

--- app/src/main/java/com/example/android/miwok/CustomFilter.java
+++ app/src/main/java/com/example/android/miwok/CustomFilter.java
@@ -7,17 +7,15 @@ package com.example.android.miwok;
 //import android.graphics.Movie;
 import android.widget.Filter;
 
-import java.text.Normalizer;
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class CustomFilter extends Filter {
 
     ArrayList<Woman> filterList;
-    WomanAdapter adapter;
+    WomenAdapterRecycle adapter;
 
-    public CustomFilter(ArrayList<Woman> filterList, WomanAdapter adapter) {
+    public CustomFilter(ArrayList<Woman> filterList, WomenAdapterRecycle adapter) {
         this.filterList = filterList;
         this.adapter = adapter;
     }
@@ -25,49 +23,49 @@ public class CustomFilter extends Filter {
     //FILTERING
     @Override
     protected FilterResults performFiltering(CharSequence constraint) {
+        FilterResults results=new FilterResults();
 
-        //RESULTS
-        FilterResults results = new FilterResults();
+        //CHECK CONSTRAINT VALIDITY
+        if(constraint != null && constraint.length() > 0)
+        {
+            //CHANGE TO UPPER
+            constraint=constraint.toString().toUpperCase();
+            //STORE OUR FILTERED PLAYERS
+            ArrayList<Woman> filteredPlayers=new ArrayList<>();
 
-        //VALIDATION
-        if (constraint != null && constraint.length() > 0) {
-
-            ArrayList<Woman> filteredWoman = new ArrayList<Woman>();
-
-            //LOOP THRU FILTER LIST
-            for (Woman row: filterList) {
-                //CHANGE TO UPPER FOR CONSISTENCY
-                String query = constraint.toString().toUpperCase();
-                //STrip accents for a accent insensitive search
-                String name = Normalizer.normalize(row.getName(), Normalizer.Form.NFD);
-                name = name.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-                query = Normalizer.normalize(query, Normalizer.Form.NFD);
-                query = query.replaceAll("\\p{InCombiningDiacriticalMarks}+", "");
-                //FILTER
-                if (name.toUpperCase().contains(query)) {
-                    filteredWoman.add(row);
+            for (int i=0;i<filterList.size();i++)
+            {
+                //CHECK
+                if(filterList.get(i).getName().toUpperCase().contains(constraint))
+                {
+                    //ADD PLAYER TO FILTERED PLAYERS
+                    filteredPlayers.add(filterList.get(i));
                 }
             }
 
-            results.count = filteredWoman.size();
-            results.values = filteredWoman;
-        } else {
-            results.count = filterList.size();
-            results.values = filterList;
+            results.count=filteredPlayers.size();
+            results.values=filteredPlayers;
+        }else
+        {
+            results.count=filterList.size();
+            results.values=filterList;
+
         }
+
 
         return results;
     }
+
 
     //PUBLISH RESULTS
 
     @Override
     protected void publishResults(CharSequence constraint, FilterResults results) {
-        adapter.setFilteredData((List<Woman>) results.values);
 
-//        adapter.women= (ArrayList<Woman>) results.values;
-//        adapter.notifyDataSetChanged();
+        adapter.women= (ArrayList<Woman>) results.values;
 
+        //REFRESH
+        adapter.notifyDataSetChanged();
     }
 }
 
