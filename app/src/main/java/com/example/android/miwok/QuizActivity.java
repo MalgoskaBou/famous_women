311a78b4793d494d084c0d4c177e0b395397f26a
Oya Canli <oyacan@gmail.com>
2018-02-27 10:51:29 +0100

    replace hashmaps with sparsearrays

--- app/src/main/java/com/example/android/miwok/QuizActivity.java
+++ app/src/main/java/com/example/android/miwok/QuizActivity.java
@@ -6,14 +6,12 @@ import android.content.res.TypedArray;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.util.SparseArray;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,9 +19,8 @@ import com.example.android.miwok.databinding.ActivityQuizBinding;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 
-    public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
+public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
 
         private final static String QUESTIONS_ARRAY_KEY = "questionsArrayKey";
         private final static String CURRENT_QUESTION = "currentQuestion";
@@ -37,11 +34,11 @@ import java.util.HashMap;
 
         float score;
         int currentQuestion, correctAnsNmb, incorrectAnsNmb, arrayLength;
-        ArrayList<QuizQuestion> questions = new ArrayList<QuizQuestion>();
-        ArrayList<Integer> wrongAnswers = new ArrayList<Integer>();
-        HashMap<Integer, RadioGroup> rgHmap;
-        HashMap<Integer, TextView> questionHmap;
-        HashMap<Integer, Button> submitHmap;
+        ArrayList<QuizQuestion> questions = new ArrayList<>();
+        ArrayList<Integer> wrongAnswers = new ArrayList<>();
+        SparseArray<RadioGroup> rgSparse;
+        SparseArray<TextView> questionSparse;
+        SparseArray<Button> submitSparse;
         boolean isResultShown;
         ActivityQuizBinding binding;
 
@@ -56,26 +53,26 @@ import java.util.HashMap;
             binding.layoutResult.setVisibility(View.GONE);
             //HashMaps pair the question numbers with the corresponding questions, answers, and submit buttons.
             // The aim is to be able to put them in a loop, so that we need significantly less lines of code.
-            rgHmap = new HashMap<Integer, RadioGroup>();
-            rgHmap.put(0, binding.rgQuestion1);
-            rgHmap.put(1, binding.rgQuestion2);
-            rgHmap.put(2, binding.rgQuestion3);
-            rgHmap.put(3, binding.rgQuestion4);
-            rgHmap.put(4, binding.rgQuestion5);
+            rgSparse = new SparseArray<>();
+            rgSparse.put(0, binding.rgQuestion1);
+            rgSparse.put(1, binding.rgQuestion2);
+            rgSparse.put(2, binding.rgQuestion3);
+            rgSparse.put(3, binding.rgQuestion4);
+            rgSparse.put(4, binding.rgQuestion5);
 
-            questionHmap = new HashMap<Integer, TextView>();
-            questionHmap.put(0,binding.tvQuestion1);
-            questionHmap.put(1,binding.tvQuestion2);
-            questionHmap.put(2,binding.tvQuestion3);
-            questionHmap.put(3,binding.tvQuestion4);
-            questionHmap.put(4,binding.tvQuestion5);
+            questionSparse = new SparseArray<>();
+            questionSparse.put(0,binding.tvQuestion1);
+            questionSparse.put(1,binding.tvQuestion2);
+            questionSparse.put(2,binding.tvQuestion3);
+            questionSparse.put(3,binding.tvQuestion4);
+            questionSparse.put(4,binding.tvQuestion5);
 
-            submitHmap = new HashMap<Integer, Button>();
-            submitHmap.put(0,binding.tvSubmit1);
-            submitHmap.put(1,binding.tvSubmit2);
-            submitHmap.put(2,binding.tvSubmit3);
-            submitHmap.put(3,binding.tvSubmit4);
-            submitHmap.put(4,binding.tvSubmit5);
+            submitSparse = new SparseArray<>();
+            submitSparse.put(0,binding.tvSubmit1);
+            submitSparse.put(1,binding.tvSubmit2);
+            submitSparse.put(2,binding.tvSubmit3);
+            submitSparse.put(3,binding.tvSubmit4);
+            submitSparse.put(4,binding.tvSubmit5);
 
             //If the activity is opened for the first time, adds all the questions to the arrayList, shuffles them and then make a sublist with the first 5 questions.
             //The questions after the first one are made invisible.
@@ -86,9 +83,9 @@ import java.util.HashMap;
                 isResultShown = false;
                 arrayLength = questions.size();
                 for(int i = 1 ; i<arrayLength ; i++){
-                    rgHmap.get(i).setVisibility(View.INVISIBLE);
-                    questionHmap.get(i).setVisibility(View.INVISIBLE);
-                    submitHmap.get(i).setVisibility(View.INVISIBLE);
+                    rgSparse.get(i).setVisibility(View.INVISIBLE);
+                    questionSparse.get(i).setVisibility(View.INVISIBLE);
+                    submitSparse.get(i).setVisibility(View.INVISIBLE);
                 }
                 //After rotation, retrieve the list of chosen questions, number of the current question, score and wrong answers up to then
                 //Redraw the current state of the views before rotation (visibilities, disabled buttons, right and wrong answer checks etc..)
@@ -108,11 +105,11 @@ import java.util.HashMap;
                     }
                 });
                 for(int j = 0; j < currentQuestion ; j++){
-                    for (int i = 0; i < rgHmap.get(j).getChildCount(); i++) {
-                        rgHmap.get(j).getChildAt(i).setEnabled(false);
+                    for (int i = 0; i < rgSparse.get(j).getChildCount(); i++) {
+                        rgSparse.get(j).getChildAt(i).setEnabled(false);
                     }
-                    correctAnswerCheck(rgHmap.get(j), j);
-                    submitHmap.get(j).setVisibility(View.GONE);
+                    correctAnswerCheck(rgSparse.get(j), j);
+                    submitSparse.get(j).setVisibility(View.GONE);
                     if(wrongAnswers.get(j) != 0){
                         RadioButton selectedAnswer = findViewById(wrongAnswers.get(j));
                         selectedAnswer.setButtonDrawable(R.drawable.ic_cancel);
@@ -124,9 +121,9 @@ import java.util.HashMap;
                 }
                 arrayLength = questions.size();
                 for(int i = currentQuestion+1 ; i<arrayLength ; i++){
-                    rgHmap.get(i).setVisibility(View.INVISIBLE);
-                    questionHmap.get(i).setVisibility(View.INVISIBLE);
-                    submitHmap.get(i).setVisibility(View.INVISIBLE);
+                    rgSparse.get(i).setVisibility(View.INVISIBLE);
+                    questionSparse.get(i).setVisibility(View.INVISIBLE);
+                    submitSparse.get(i).setVisibility(View.INVISIBLE);
                 }
 
                 // Display result
@@ -137,10 +134,10 @@ import java.util.HashMap;
 
             // Display questions and answers
             for(int j = 0; j< arrayLength; j++){
-                questionHmap.get(j).setText(questions.get(j).getQuestion());
-                RadioButton option1 = (RadioButton) rgHmap.get(j).getChildAt(1);
-                RadioButton option2 = (RadioButton) rgHmap.get(j).getChildAt(2);
-                RadioButton option3 = (RadioButton) rgHmap.get(j).getChildAt(3);
+                questionSparse.get(j).setText(questions.get(j).getQuestion());
+                RadioButton option1 = (RadioButton) rgSparse.get(j).getChildAt(1);
+                RadioButton option2 = (RadioButton) rgSparse.get(j).getChildAt(2);
+                RadioButton option3 = (RadioButton) rgSparse.get(j).getChildAt(3);
                 option1.setText(questions.get(j).getAnswer1());
                 option2.setText(questions.get(j).getAnswer2());
                 option3.setText(questions.get(j).getAnswer3());
@@ -181,51 +178,51 @@ import java.util.HashMap;
 
             // Randomized questions
             Collections.shuffle(questions);
-            questions = new ArrayList<QuizQuestion>(questions.subList(0,5));
+            questions = new ArrayList<>(questions.subList(0,5));
             return questions;
         }
 
         //Common submit method for all questions
         public void submit(int numberOfQuestion) {
             //Warn if no answer is selected
-            if (rgHmap.get(numberOfQuestion).getCheckedRadioButtonId() == -1) {
+            if (rgSparse.get(numberOfQuestion).getCheckedRadioButtonId() == -1) {
                 Toast.makeText(getBaseContext(), "Select answer!", Toast.LENGTH_SHORT).show();
                 return;
             } else {
                 //Correct option is checked whether user gives the right answer or not
-                int selectedRadioButtonID = rgHmap.get(numberOfQuestion).indexOfChild(findViewById(rgHmap.get(numberOfQuestion).getCheckedRadioButtonId()));
-                correctAnswerCheck(rgHmap.get(numberOfQuestion), numberOfQuestion);
+                int selectedRadioButtonID = rgSparse.get(numberOfQuestion).indexOfChild(findViewById(rgSparse.get(numberOfQuestion).getCheckedRadioButtonId()));
+                correctAnswerCheck(rgSparse.get(numberOfQuestion), numberOfQuestion);
                 //if the answer was wrong put a wrong symbol to the option chosen.
                 if (questions.get(numberOfQuestion).getCorrectAnswer() != selectedRadioButtonID) {
-                    incorrectAnswerCheck(rgHmap.get(numberOfQuestion));
+                    incorrectAnswerCheck(rgSparse.get(numberOfQuestion));
                     //Keep track of wrong answers for rotation
-                    wrongAnswers.add(rgHmap.get(numberOfQuestion).getCheckedRadioButtonId());
+                    wrongAnswers.add(rgSparse.get(numberOfQuestion).getCheckedRadioButtonId());
                 } else {
                     score++;
                     wrongAnswers.add(0);
                 }
                 // For non-checked radio button show ic_notchecked
-                for (int i = 1; i < rgHmap.get(numberOfQuestion).getChildCount(); i++){
+                for (int i = 1; i < rgSparse.get(numberOfQuestion).getChildCount(); i++){
                     if (i != selectedRadioButtonID && i != questions.get(numberOfQuestion).getCorrectAnswer()){
-                        notCheck(rgHmap.get(numberOfQuestion), i);
+                        notCheck(rgSparse.get(numberOfQuestion), i);
                     }
                 }
 
                 //Disable the previous question once it is submitted
-                for (int i = 0; i < rgHmap.get(numberOfQuestion).getChildCount(); i++) {
-                    rgHmap.get(numberOfQuestion).getChildAt(i).setEnabled(false);
+                for (int i = 0; i < rgSparse.get(numberOfQuestion).getChildCount(); i++) {
+                    rgSparse.get(numberOfQuestion).getChildAt(i).setEnabled(false);
                 }
-                submitHmap.get(numberOfQuestion).setVisibility(View.GONE);
+                submitSparse.get(numberOfQuestion).setVisibility(View.GONE);
                 //Make the next question visible
                 numberOfQuestion++;
                 if(numberOfQuestion<arrayLength){
-                    questionHmap.get(numberOfQuestion).setVisibility(View.VISIBLE);
-                    rgHmap.get(numberOfQuestion).setVisibility(View.VISIBLE);
-                    submitHmap.get(numberOfQuestion).setVisibility(View.VISIBLE);
+                    questionSparse.get(numberOfQuestion).setVisibility(View.VISIBLE);
+                    rgSparse.get(numberOfQuestion).setVisibility(View.VISIBLE);
+                    submitSparse.get(numberOfQuestion).setVisibility(View.VISIBLE);
                 }
                 // Automatically scroll to next question's submit button
                 if (numberOfQuestion < arrayLength)
-                    submitHmap.get(numberOfQuestion).getParent().requestChildFocus(submitHmap.get(numberOfQuestion), submitHmap.get(numberOfQuestion));
+                    submitSparse.get(numberOfQuestion).getParent().requestChildFocus(submitSparse.get(numberOfQuestion), submitSparse.get(numberOfQuestion));
                 // Last question - display result
                 if (numberOfQuestion == arrayLength){
                     score = score / 5 * 100;
@@ -239,7 +236,7 @@ import java.util.HashMap;
         }
 
         public void correctAnswerCheck(RadioGroup rg, int numberOfQuestion) {
-            RadioButton correctAnswer = (RadioButton) findViewById(rg.getChildAt(questions.get(numberOfQuestion).getCorrectAnswer()).getId());
+            RadioButton correctAnswer = findViewById(rg.getChildAt(questions.get(numberOfQuestion).getCorrectAnswer()).getId());
             correctAnswer.setButtonDrawable(R.drawable.ic_check);
             LinearLayout.LayoutParams params1 = (LinearLayout.LayoutParams) correctAnswer.getLayoutParams();
             params1.setMargins(16, 0, 0, 0);
@@ -249,7 +246,7 @@ import java.util.HashMap;
 
         public void incorrectAnswerCheck(RadioGroup rg) {
 
-            RadioButton selectedAnswer = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+            RadioButton selectedAnswer = findViewById(rg.getCheckedRadioButtonId());
             selectedAnswer.setButtonDrawable(R.drawable.ic_cancel);
             LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) selectedAnswer.getLayoutParams();
             params.setMargins(16, 0, 0, 0);
@@ -270,7 +267,7 @@ import java.util.HashMap;
         public int correctAnswersNmb() {
             int correctNmb = 0;
             for (int i = 0; i < arrayLength; i++){
-                int selectedRadioButtonID = rgHmap.get(i).indexOfChild(findViewById(rgHmap.get(i).getCheckedRadioButtonId()));
+                int selectedRadioButtonID = rgSparse.get(i).indexOfChild(findViewById(rgSparse.get(i).getCheckedRadioButtonId()));
                 if (selectedRadioButtonID == questions.get(i).getCorrectAnswer()){
                     correctNmb ++;
                 }
